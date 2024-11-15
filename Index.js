5279a2de932c190372783d4eea7e517784bb9a92
Jafar <134295552+jafar16@users.noreply.github.com>
2024-11-15 15:15:30 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.getElementById("login-form").classList.remove("hidden");
+    document.getElementById("signup-form").classList.add("hidden");
+}
+
+function showSignUp() {
+    document.getElementById("signup-form").classList.remove("hidden");
+    document.getElementById("login-form").classList.add("hidden");
+}
